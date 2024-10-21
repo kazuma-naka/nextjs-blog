608d54b4a9faf832635f96c40d77cd612a015beb
kazuma-naka <kazuma.naka.01@gmail.com>
2024-10-21 12:17:14 -0400

    add css file for layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import styles from "../styles/layout.module.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <h2>共通ヘッダー</h2>
-      <Image src="/dogs.jpg" height={144} width={144} alt="Dog Image"></Image>
+      <h2 className={styles.commonHeader}>共通ヘッダー</h2>
+      <Image
+        className={styles.dogImage}
+        src="/dogs.jpg"
+        height={144}
+        width={144}
+        alt="Dog Image"
+      />
       {children}
     </>
   );
