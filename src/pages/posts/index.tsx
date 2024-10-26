4003b3984959983869c687cd9856e84aba8d57ca
kazuma-naka <kazuma.naka.01@gmail.com>
2024-10-26 09:52:14 -0400

    index.tsx を posts 下に作成して固定データとして post のリストをプレレンダリングする
    index.tsx は Link Component で `/post` を使用する

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,38 @@
+import { GetStaticProps, NextPage } from "next/types";
+
+type Post = {
+  id: number;
+  title: string;
+  date: string;
+};
+
+type HomeProps = {
+  allPostsData: Post[];
+};
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  return {
+    props: {
+      allPostsData: [
+        { id: 1, title: "First Post", date: "2024-01-01" },
+        { id: 2, title: "Second Post", date: "2024-01-02" },
+        { id: 3, title: "Third Post", date: "2024-01-03" },
+      ],
+    },
+  };
+};
+
+const Home: NextPage<HomeProps> = ({ allPostsData }) => {
+  return (
+    <>
+      {allPostsData?.map(({ id, title, date }) => (
+        <div key={id}>
+          <p>
+            date: {date}, title: {title}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+};
+export default Home;
